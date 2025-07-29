6f634b2050703710536a2cb0e42b9ca36ac68ad7
Vyzecb <info@vyze.nl>
2025-07-29 21:16:19 +0200

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
-import { AppProps } from 'next/app';
-import Head from 'next/head';
-import { ThemeProvider } from '@/context/ThemeContext';
-import '../styles/globals.css';
+// src/pages/_app.tsx
+import '../styles/globals.css'                // Tailwind + custom global styles
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { ThemeProvider } from '@/context/ThemeContext'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -10,13 +11,17 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta charSet="UTF-8" />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+        <meta
+          name="description"
+          content="Ervaar een luxe 3D-webervaring met Three.js, GSAP en Next.js"
         />
         <title>My Luxury 3D Site</title>
       </Head>
       <Component {...pageProps} />
     </ThemeProvider>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
