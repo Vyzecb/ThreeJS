f319d162a6f458ffb94dcd8bcc9e3aafa4a7b1cd
Vyzecb <info@vyze.nl>
2025-07-29 20:58:59 +0200

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+// src/components/Layout.tsx
+import React, { PropsWithChildren, FC } from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import ScrollIndicator from './ScrollIndicator';
 
-const Layout: React.FC = ({ children }) => (
-  <>
+const Layout: FC<PropsWithChildren<{}>> = ({ children }) => (
+  <div className="flex flex-col min-h-screen bg-secondary text-primary">
     <Header />
-    <main>{children}</main>
+    <main className="flex-grow">{children}</main>
+    <ScrollIndicator />
     <Footer />
-  </>
+  </div>
 );
 
 export default Layout;
