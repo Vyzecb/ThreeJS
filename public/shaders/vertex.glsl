42a2e35e898bb78bec3817936d76bbe22b562a58
Vyzecb <info@vyze.nl>
2025-07-29 22:09:43 +0200

    Update vertex.glsl

--- public/shaders/vertex.glsl
+++ public/shaders/vertex.glsl
@@ -1,4 +1,27 @@
-// Vertex shader placeholder
+// Vertex Shader
+precision mediump float;
+
+attribute vec3 position;
+attribute vec3 normal;
+attribute vec2 uv;
+
+uniform mat4 modelViewMatrix;
+uniform mat4 projectionMatrix;
+uniform mat3 normalMatrix;
+
+varying vec2 vUv;
+varying vec3 vNormal;
+varying vec3 vViewPosition;
+
 void main() {
-  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  // Pass UV and normal to fragment
+  vUv = uv;
+  vNormal = normalize( normalMatrix * normal );
+
+  // Compute view‐space position
+  vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
+  vViewPosition = -mvPosition.xyz;
+
+  // Standard transform
+  gl_Position = projectionMatrix * mvPosition;
 }
