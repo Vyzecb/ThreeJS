84e97b144d5ddbb912b9567cb871a2b4de2b4d30
Vyzecb <info@vyze.nl>
2025-07-29 21:02:02 +0200

    Update ScrollIndicator.tsx

--- src/components/ScrollIndicator.tsx
+++ src/components/ScrollIndicator.tsx
@@ -1,3 +1,4 @@
+// src/components/ScrollIndicator.tsx
 import React, { useEffect, useRef } from 'react';
 import { gsap } from 'gsap';
 
@@ -6,15 +7,35 @@ const ScrollIndicator: React.FC = () => {
 
   useEffect(() => {
     if (!arrowRef.current) return;
-    const tl = gsap.timeline({ repeat: -1, yoyo: true, defaults: { ease: 'power1.inOut' } });
-    tl.to(arrowRef.current, { y: 10, autoAlpha: 1, duration: 0.8 })
-      .to(arrowRef.current, { y: 0, autoAlpha: 0.5, duration: 0.8 });
-    return () => tl.kill();
+
+    const tl = gsap.timeline({ 
+      repeat: -1, 
+      yoyo: true, 
+      defaults: { ease: 'power1.inOut' } 
+    });
+
+    tl.to(arrowRef.current, {
+      y: 10,
+      autoAlpha: 1,
+      duration: 0.8,
+    })
+    .to(arrowRef.current, {
+      y: 0,
+      autoAlpha: 0.5,
+      duration: 0.8,
+    });
+
+    return () => {
+      tl.kill();  // blokhaken zodat deze functie void teruggeeft
+    };
   }, []);
 
   return (
     <div className="fixed bottom-8 left-1/2 transform -translate-x-1/2 z-50 pointer-events-none">
-      <div ref={arrowRef} className="w-6 h-6 border-b-2 border-r-2 border-secondary transform rotate-45 opacity-0" />
+      <div
+        ref={arrowRef}
+        className="w-6 h-6 border-b-2 border-r-2 border-secondary transform rotate-45 opacity-0"
+      />
     </div>
   );
 };
