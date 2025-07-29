2ba39043b90d02b44708152bcc985b8629752ee2
Vyzecb <info@vyze.nl>
2025-07-29 21:23:42 +0200

    Update ScrollIndicator.tsx

--- src/components/ScrollIndicator.tsx
+++ src/components/ScrollIndicator.tsx
@@ -8,33 +8,36 @@ const ScrollIndicator: React.FC = () => {
   useEffect(() => {
     if (!arrowRef.current) return;
 
-    const tl = gsap.timeline({ 
-      repeat: -1, 
-      yoyo: true, 
-      defaults: { ease: 'power1.inOut' } 
+    const tl = gsap.timeline({
+      repeat: -1,
+      yoyo: true,
+      defaults: { ease: 'power1.inOut' },
     });
 
     tl.to(arrowRef.current, {
       y: 10,
       autoAlpha: 1,
       duration: 0.8,
-    })
-    .to(arrowRef.current, {
+    }).to(arrowRef.current, {
       y: 0,
       autoAlpha: 0.5,
       duration: 0.8,
     });
 
     return () => {
-      tl.kill();  // blokhaken zodat deze functie void teruggeeft
+      tl.kill();
     };
   }, []);
 
   return (
-    <div className="fixed bottom-8 left-1/2 transform -translate-x-1/2 z-50 pointer-events-none">
+    <div className="fixed bottom-4 sm:bottom-8 left-1/2 transform -translate-x-1/2 z-50 pointer-events-none">
       <div
         ref={arrowRef}
-        className="w-6 h-6 border-b-2 border-r-2 border-secondary transform rotate-45 opacity-0"
+        className="
+          w-4 h-4 sm:w-6 sm:h-6 
+          border-b-2 border-r-2 border-secondary 
+          transform rotate-45 opacity-0
+        "
       />
     </div>
   );
